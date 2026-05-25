65bb6e670f7d9e6b7508b9dd69314a6f2464a1d7
johnfenner <johnfenner17@gmail.com>
2026-05-25 08:09:38 -0500

    Add files via upload

--- views/ucm/ucm.py
+++ views/ucm/ucm.py
@@ -1,10 +1,8 @@
 # Archivo: views/total.py
 import streamlit as st
-# import pandas as pd  <-- Tus otras importaciones van aquí arriba
 
 def mostrar_vista():
-    # TODO TU CÓDIGO DEBE IR AQUÍ ADENTRO (Indentado)
+    # TODO 
     st.title("📊 Vista Total del Holding")
     st.write("Aquí va el contenido consolidado de todas las sedes...")
     
-    # ... resto de tus gráficas, métricas y tablas para esta vista
