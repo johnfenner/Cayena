ecbc061b3f40240c3e07349278480801e9d8c783
johnfenner <johnfenner17@gmail.com>
2026-05-25 07:37:16 -0500

    Add files via upload

---
+++ views/hacp/hacp.py
@@ -0,0 +1,10 @@
+# Archivo: views/total.py
+import streamlit as st
+# import pandas as pd  <-- Tus otras importaciones van aquí arriba
+
+def mostrar_vista():
+    # TODO TU CÓDIGO DEBE IR AQUÍ ADENTRO (Indentado)
+    st.title("📊 Vista HACP Putumayo")
+    st.write("Aquí va el contenido consolidado de todas las sedes...")
+    
+    # ... resto de tus gráficas, métricas y tablas para esta vista
