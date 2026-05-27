b851697aac119f4dafa81cc16b923396229fd7af
johnfenner <johnfenner17@gmail.com>
2026-05-27 14:22:28 -0500

    Add files via upload

--- views/hact/hact.py
+++ views/hact/hact.py
@@ -1,8 +1,40 @@
-# Archivo: views/total.py
 import streamlit as st
 
+from .informe_general import mostrar_informe_general
+from .entidades import mostrar_entidades
+from .unidades_funcionales import mostrar_unidades_funcionales
+from .mercadeo import mostrar_mercadeo 
+
 def mostrar_vista():
-    # TODO 
-    st.title("📊 Vista Total del Holding")
-    st.write("Aquí va el contenido consolidado de todas las sedes...")
+    # 1. Configuración de la página
+    st.set_page_config(page_title="Informe General - Consumo Diario", layout="wide")
     
+    col_izq, col_logo, col_der = st.columns([1, 2, 1])
+    
+    with col_logo:
+        st.image("assets/images/logo_hact_tolima_grande.webp")
+    
+    # 2. Creación de las Pestañas 
+    tab1, tab2, tab3, tab4 = st.tabs([
+        "INFORME GENERAL", 
+        "ENTIDADES", 
+        "UNIDADES FUNCIONALES", 
+        "MERCADEO"  
+    ])
+    
+    # 3. Asignación de contenido modularizado a cada pestaña
+    with tab1:
+        mostrar_informe_general()
+        
+    with tab2:
+        mostrar_entidades()
+        
+    with tab3:
+        mostrar_unidades_funcionales()
+        
+    with tab4:
+        mostrar_mercadeo() 
+    
+
+if __name__ == "__main__":
+    mostrar_vista()
